7c4f50dbfface1a669e3817fb414ec7a59ae01d4
K Hong <albtrossfacefall@gmail.com>
2019-08-23 08:11:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,5 +15,13 @@ class Song
     @artist=artist
     artist.songs.push(self)
   end
+  
+  def artist_name
+    if self.artist==nil
+      nil
+    else
+      self.artist.name
+    end
+  end
 
 end
